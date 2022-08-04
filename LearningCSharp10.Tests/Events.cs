835153a1d3d5af4393c0fd1df27b121dea60ec46
Dmitrii Radin <rdima.9618@gmail.com>
2022-08-04 15:26:03 +0400

    Delegates, Events, Tuples

---
+++ LearningCSharp10.Tests/Events.cs
@@ -0,0 +1,37 @@
+﻿namespace LearningCSharp10.Tests;
+
+public class Events
+{
+    private delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
+
+    private class ItemStore
+    {
+        public event PriceChangedHandler PriceChanged = null!;
+
+        public void ChangePrice(decimal oldPrice, decimal newPrice)
+        {
+            PriceChanged?.Invoke(oldPrice, newPrice);
+        }
+    }
+
+    [Test]
+    public void Method()
+    {
+        var itemStore = new ItemStore();
+
+        itemStore.PriceChanged += OnPriceChanged;
+
+        var isInvoked = false;
+
+        itemStore.ChangePrice(10, 20);
+
+        isInvoked.Should().BeTrue();
+
+        void OnPriceChanged(decimal oldPrice, decimal newPrice)
+        {
+            isInvoked = true;
+            oldPrice.Should().Be(10);
+            newPrice.Should().Be(20);
+        }
+    }
+}
---
+++ LearningCSharp10.Tests/Tuples.cs
@@ -0,0 +1,36 @@
+﻿namespace LearningCSharp10.Tests;
+
+public class Tuples
+{
+    [Test]
+    public void CreateTupleType()
+    {
+        var (name, age) = (Name: "Bob", Age: 23);
+
+        name.Should().Be("Bob");
+        age.Should().Be(23);
+    }
+
+    [Test]
+    public void ReturnTupleTypeFromFunction()
+    {
+        var (name, age) = GetUser();
+
+        name.Should().Be("Bob");
+        age.Should().Be(23);
+
+        static (string Name, int Age) GetUser()
+        {
+            return ("Bob", 23);
+        }
+    }
+
+    [Test]
+    public void CompareTuplesByUnderlyingData()
+    {
+        var t1 = ("one", 1);
+        var t2 = ("one", 1);
+
+        (t1 == t2).Should().BeTrue();
+    }
+}
